df9a3ab838245a915da871bbd6ca0387f2b932d7
Kamil Marynowski <marynovski@gmail.com>
2017-11-19 13:04:07 +0100

    0.1.1
    Poprawiona walidacja pustych pól w formularzu

--- walidacja.php
+++ walidacja.php
@@ -12,31 +12,40 @@ else{
 
 //imie, nazwisko, adres, miasto, kodpocztowy, nrtel, email  /*** SPRAWDZANIE CZY UŻYTKOWNIK PODAŁ DANE ***/
 
-if(empty($_POST['imie']))
+$blad = false;
+
+if(empty($_POST['imie'])){
 	$_SESSION['emptyimie'] = '<span class="error">Musisz podać imię!</span>';
+	$blad = true;
+}
 
-if(empty($_POST['nazwisko']))
+if(empty($_POST['nazwisko'])){
 	$_SESSION['emptynazwisko'] = '<span class="error">Musisz podać nazwisko!</span>';
-
-if(empty($_POST['adres']))
+	$blad = true;
+}
+if(empty($_POST['adres'])){
 	$_SESSION['emptyadres'] = '<span class="error">Musisz podać adres!</span>';
-
-if(empty($_POST['miasto']))
+	$blad = true;
+}
+if(empty($_POST['miasto'])){
 	$_SESSION['emptymiasto'] = '<span class="error">Musisz podać miasto!</span>';
-
-if(empty($_POST['kodpocztowy']) || empty($_POST['kodpocztowy2']))
+	$blad = true;
+}
+if(empty($_POST['kodpocztowy']) || empty($_POST['kodpocztowy2'])){
 	$_SESSION['emptykodpocztowy'] = '<span class="error">Musisz podać kodpocztowy!</span>';
-
-if(empty($_POST['nrtel']))
+	$blad = true;
+}
+if(empty($_POST['nrtel'])){
 	$_SESSION['emptynrtel'] = '<span class="error">Musisz podać nr tel!</span>';
-
-if(empty($_POST['email']))
+	$blad = true;
+}
+if(empty($_POST['email'])){
 	$_SESSION['emptyemail'] = '<span class="error">Musisz podać email!</span>';
-
+	$blad = true;
+}
 //JESLI NIE PODAL KTOREJKOLWIEK SKRYPT PRZENOSI GO DO FORMULARZA GDZIE WYSWIETLA STOSOWNE KOMUNIKATY
 
-if(empty($_POST['imie']) || empty($_POST['nazwisko']) || empty($_POST['adres']) || empty($_POST['miasto']) || empty($_POST['kodpocztowy']) || empty($_POST['nrtel']) || empty($_POST['email'])){
-
+if($blad){
 	header("location: formularz.php");
 	exit();
 }
